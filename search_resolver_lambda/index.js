8565076ddc523c61f360b66afb77ddecf5c6f286
Kenneth Li <kenkraken@gmail.com>
2020-02-09 13:27:04 -0600

    Update lambda to act as proxy for web service

--- search_resolver_lambda/index.js
+++ search_resolver_lambda/index.js
@@ -1,10 +1,8 @@
 const es = require("elasticsearch");
 const awsES = require("http-aws-es");
-const Ajv = require('ajv');
 
 const ES_DOMAIN_URL = process.env.ES_DOMAIN_URL;
 const ES_INDEX = process.env.ES_INDEX;
-const Schema = require('./schema');
 
 exports.handler = (event, context, callback) => {
   const done = (err, res) =>
@@ -24,91 +22,21 @@ exports.handler = (event, context, callback) => {
     done(new Error(`Invalid Elasticsearch index name "${ES_INDEX}`));
   }
 
-  async function singleQueryES(phrase, period, category) {
-    const start = Math.trunc(period.start);
-    const end = Math.trunc(period.end);
-    const interval = Math.trunc(period.interval)*1000; //Input is milliseconds
+  async function singleQueryES(query) {
     const esClient = new es.Client({
       hosts: [ES_DOMAIN_URL],
       connectionClass: awsES
     });
 
-    // ES 2.3 Query and Aggregations
-    // https://www.elastic.co/guide/en/elasticsearch/reference/2.3/query-filter-context.html
-    // https://www.elastic.co/guide/en/elasticsearch/reference/2.3/search-aggregations-bucket-datehistogram-aggregation.html
-
-    const queryBody = {
-      _source: false,
-      size: 0,
-      query: {
-        bool: {
-          must: {
-            match_phrase: {
-              "fields.abstract": phrase
-            }
-          },
-          filter: {
-            range: {
-              "fields.published_date": {
-                gte: start,
-                lte: end
-              }
-            }
-          }
-        }
-      },
-
-      aggs: {
-        articles_over_time: {
-          histogram: {
-            field: "fields.published_date",
-            interval: interval
-          }
-        }
-      }
-    };
     return esClient.search({
       index: ES_INDEX,
-      body: queryBody
+      body: query
     });
   }
 
-  async function queryES(req) {
-    return Promise.all(
-      req["phrases"].map(phrase =>
-        singleQueryES(phrase, req.period, req.category)
-      )
-    );
-  }
-
-  function transformAgg(data) {
-    return data.aggregations.articles_over_time.buckets.map(bucket => ({
-      t: parseInt(bucket.key_as_string),
-      y: bucket.doc_count
-    }));
-  }
-
   async function getESResponse(req) {
-    validateInput(req);
-    const res = await queryES(req);
-    console.log(res);
-    const resFiltered = res
-      .map((obj, i) => ({
-        phrase: req.phrases[i],
-        data: obj
-      }))
-      .map(markedObj => ({
-        phrase: markedObj.phrase,
-        data: transformAgg(markedObj.data)
-      }));
-    return resFiltered;
-  }
-
-  function validateInput(body) {
-    const ajv = new Ajv();
-    const validate = ajv.compile(Schema);
-    const valid = validate(body);
-    if (!valid) done(new Error(`Error validating body format: "${ajv.errors}`));
+    const results = await Promise.all(req.map(query => singleQueryES(query)));
+    return results;
   }
 
   switch (event.httpMethod) {
